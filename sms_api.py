4e7e7bb30062f64701e2e5866a2344004241a38d
qchenevier <qchenevier@users.noreply.github.com>
2022-06-24 12:04:05 +0200

    SMS is not mandatory anymore

--- sms_api.py
+++ sms_api.py
@@ -2,14 +2,23 @@ import json
 
 from twilio.rest import Client
 
-with open("twilio_config.json", "r") as f:
-    twilio_config = json.load(f)
+from pathlib import Path
 
-client = Client(twilio_config["account_sid"], twilio_config["auth_token"])
+twilio_config_path = Path(__file__).parent / "twilio_config.json"
+
+if twilio_config_path.exists():
+    with open(twilio_config_path, "r") as f:
+        twilio_config = json.load(f)
+    client = Client(twilio_config["account_sid"], twilio_config["auth_token"])
+else:
+    client = False
 
 
 def send_sms(text="Hello world"):
-    message = client.messages.create(
-        to=twilio_config["to"], from_=twilio_config["from_"], body=text
-    )
-    print(message.sid)
+    if client:
+        message = client.messages.create(
+            to=twilio_config["to"], from_=twilio_config["from_"], body=text
+        )
+        print(message.sid)
+    else:
+        print("Cannot send sms: no sms configuration.")
